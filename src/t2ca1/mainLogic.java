97cb3562015e8d389d6ff1ffa7516b437c06b04b
Evgeniy <sba23066@student.cct.ie>
2024-01-18 20:49:12 +0000

    next update

--- src/t2ca1/mainLogic.java
+++ src/t2ca1/mainLogic.java
@@ -19,35 +19,101 @@ import java.util.Scanner;
 
 public class mainLogic {
     private String myInFile = "students.txt";
+    private String myOutFile = "status.txt";
+    
     private BufferedWriter outputF;
     private BufferedReader inputF;
-    private int count = 0;
+    private int total_count = 0;
     private boolean verbal = true;
+    private int local_count = 1;
 
     public mainLogic(){
         // Init
     }    
     
     public boolean treatFile(){
-        // ==== Read current trade rates ===
+       
+        total_count = 0;
+        
         try {
-            inputF = new BufferedReader(new FileReader(myInFile));
+            inputF = new BufferedReader(new FileReader(myInFile)); // Try open the file
         } catch (Exception e) {
             System.out.println("Open file error: " + e );
             return false;
         }
 
+//        Sam Weiss
+//        5
+//        22DIP1123
+//        Steve Rodgers
+//        7
+//        20MSC1914
+        
         String line;
+        
+        int index_firstName  = 0;
+        int index_secondName  = 1;
+        boolean canGoOn = true;
         try {
             line = inputF.readLine();
             while (line != null) {
                 
-                count++;
-                if (verbal) System.out.print(count + ") " + line + " ");
-                System.out.println("" );
+                if (!line.trim().isEmpty()) { //Check for empty line and skip it
+
+                    if (verbal) System.out.print(local_count + ") " + line + " "); // Show line for user and apopriate error if there is
+                    
+                    switch (local_count) {
+                        case 1:
+                            String[] studentCredians = line.split(" ");
+                            
+                            if (verbal) System.out.print("Line 1");
+                            
+                            // condition 1b check
+                            if (studentCredians.length<=1) {
+                                 System.out.println("[Error] the second name must be separated from the first name by a single space");
+                                 canGoOn = false;
+                                 //continue;
+                            }
+
+                            // condition 1a check 
+                            if (!(studentCredians[index_firstName].matches("\\w+\\.?"))) {
+                                System.out.println("[Error] the first name must be letters only");
+                                canGoOn = false;
+                                //continue;
+                            }
+
+                            // condition 1b check 
+                            if (!(studentCredians[index_secondName].matches("[A-Z0-9]+"))) {
+                                System.out.println("[Error] the second name can be letters and/or numbers");
+                                canGoOn = false;
+                                //continue;
+                            }                            
+                            break;
+                            
+                        case 2: // condition 1c: the number of classes must be an integer value between 1 and 8 (inclusive)
+                            if (verbal) System.out.println("Line 2");
+                            
+                            break;
+                        
+                        case 3: // 1d)	the student “number” must be a minimum of 6 characters with the first 2 characters being numbers, 
+                                // the 3rd  and 4th characters (and possibly 5th ) being a letter, and everything after the last letter character being a number. 
+                            if (verbal) System.out.println("Line 3");
+                            
+                            break;                            
+                    }
+                    
+
+                    local_count++;
+                    if(local_count>3) {
+                        local_count = 1;
+                        System.out.println("");
+                    }
+
+                }
                 
-                line = inputF.readLine();
-                
+                if (canGoOn) System.out.println(" [OK]"); 
+                line = inputF.readLine(); // Read next line from the file
+                total_count++;
             }
             
         } catch (Exception e) {
