cbb98b1506b2cecade56372cd067fa6b766999c4
Evgeniy <sba23066@student.cct.ie>
2024-01-19 11:53:23 +0000

    next update

--- src/t2ca1/mainLogic.java
+++ src/t2ca1/mainLogic.java
@@ -28,7 +28,11 @@ public class mainLogic {
     private int total_count;
     private boolean verbal = true;
     private int local_count;
-    private boolean canWrite;
+    public boolean canWrite;
+    
+    public String studentWorkload ="";
+    public String studentSecondName ="";
+    public String StudentNumber = ""; 
     
     private String Regex_combination_of_letters_or_numbers = "^[a-zA-Z0-9]+$"; //"^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$";
     private String Regex_just_letters = "^(?=.*[a-zA-Z])[a-zA-Z]+$"; //"\\w+\\.?"
@@ -44,13 +48,8 @@ public class mainLogic {
         local_count = 1;
         String line;
         
-        int index_firstName  = 0;
-        int index_secondName  = 1;
-        
-        int studentClass;
-        String studentWorkload ="";
-        String studentSecondName ="";
-        String StudentNumber = "";       
+      
+            
         
         try {
             inputF = new BufferedReader(new FileReader(myInFile)); // Try to open the raw students' data file
@@ -71,85 +70,26 @@ public class mainLogic {
                     if (verbal) System.out.print(local_count + ") " + line + " "); // Show line for user and apopriate error if there is
                     
                     switch (local_count) {
-                        case 1:     // Validate the first line of a student's data record
-                            String[] studentCredians = line.trim().split(" ");
-                            studentSecondName = studentCredians[index_secondName];
-                            // condition 1b check
-                            if (studentCredians.length<=1) {
-                                logShowErr("[Error] the second name must be separated from the first name by a single space");
-                            } else {
-                                // condition 1a check 
-                                if (!(studentCredians[index_firstName].matches(Regex_just_letters))) { 
-                                    logShowErr("[Error] the first name must be letters only");
-                                } else {
-                                    // condition 1b check 
-                                    if (!(studentSecondName.matches(Regex_combination_of_letters_or_numbers))) {
-                                        logShowErr("[Error] the second name can be letters and/or numbers");
-                                    }          
-                                }
-                            }
+                        case 1: // Validate the first line of a student's data record
+                            validateL1(line);
                             break;
                             
                         case 2: // Validate the second line of a student's data record
-                            studentClass = Integer.parseInt(line);
-                            if (studentClass<1 || studentClass>8) { // condition 1c check 
-                                logShowErr("[Error] the number of classes must be an integer value between 1 and 8 (inclusive)");
-                            } else {
-                                studentWorkload = getStudentWorkload(studentClass);
-                                if (verbal) System.out.print(" [" + studentWorkload + "] " );
-                            }
+                            validateL2(line);
                             break;
                         
                         case 3: // Validate the third line of a student's data record
-
-                                // the 3rd  and 4th characters (and possibly 5th ) being a letter, and everything after the last letter character being a number. 
-                            if (line.trim().length()<6) { // 1d check
-                                logShowErr("[Error] the student “number” must be a minimum of 6 characters ");
-                            } else {
-                                String first2characters_numbers = line.substring(0, 2);
-                                int studentYear = Integer.parseInt(first2characters_numbers); //with the first 2 characters being numbers, 
-                                if (studentYear<20) { // DW1 check: Ensure that the student number year is at least 2020 (i.e. that the number starts with 20 or higher)
-                                    logShowErr("[Error] the year is not valid in students's number (Must be 20 or higher)");
-                                } else {
-                                    char L1 = line.charAt(2);
-                                    char L2 = line.charAt(3);
-                                    char L3 = line.charAt(4);
-                                    
-                                    if (Character.isLetter(L1) && Character.isLetter(L2)){
-                                        
-                                        int pos = 4;
-                                        if (Character.isLetter(L3)) pos = 5;
-                                        String reasonableNumber = line.substring(pos);
-                                        int intReasonableNumber = Integer.parseInt(reasonableNumber);
-                                        if (intReasonableNumber<1 || intReasonableNumber>200) {
-                                            logShowErr("[Error] students's number " + intReasonableNumber + " is not reasonable (must be between 1 and 200)");
-                                        } else {
-                                            System.out.println(first2characters_numbers + " " + reasonableNumber);
-                                            StudentNumber = line;
-                                        }
-                                        
-                                    } else {
-                                        logShowErr("[Error] the 3rd  and 4th characters (and possibly 5th ) in student's number must be a letter");
-                                    }
-                                    
-                                }
-                            }
-                            
-                            
+                            validateL3(line);
                             break;                            
                     }
                     
-                    if (canWrite) {
-                        if (verbal) System.out.println(" [OK]");
-                    } 
-
+                    if (canWrite && verbal) System.out.println(" [OK]");
                 }
                 
                 local_count++;              // Increase file line counter
                 if(local_count>3) {         // If we've read 3 lines (student's record) go on to next one
                     local_count = 1;
                     System.out.println("");
-                    System.out.println("canWrite:" + canWrite);
                     if(canWrite) write2File(StudentNumber + " - " + studentSecondName + "\n" +  studentWorkload, true );
                     canWrite = true;         // Reset flag
                 }                
@@ -165,6 +105,87 @@ public class mainLogic {
         return true;
     }
 
+    public void validateL1(String line){
+        int index_firstName  = 0;
+        int index_secondName  = 1;
+        
+        String[] studentCredians = line.trim().split(" ");
+        studentSecondName = studentCredians[index_secondName];
+        
+        if (studentCredians.length<=1) { // condition 1b check
+            logShowErr("[Error] the second name must be separated from the first name by a single space");
+        } else {
+            
+            if (!(studentCredians[index_firstName].matches(Regex_just_letters))) { // condition 1a check  
+                logShowErr("[Error] the first name must be letters only");
+            } else {
+                
+                if (!(studentSecondName.matches(Regex_combination_of_letters_or_numbers))) { // condition 1b check 
+                    logShowErr("[Error] the second name can be letters and/or numbers");
+                }          
+            }
+        }
+    }
+    
+    public boolean isNumeric(String str) { 
+        try {  
+          Double.parseDouble(str);  
+          return true;
+        } catch(Exception e){  
+          return false;  
+        }  
+      }
+    
+    public void validateL2(String line){
+        int studentClass;
+        if (isNumeric(line)) {
+            studentClass = Integer.parseInt(line);
+            if (studentClass<1 || studentClass>8) { // condition 1c check 
+                logShowErr("[Error] the number of classes must be an integer value between 1 and 8 (inclusive)");
+            } else {
+                studentWorkload = getStudentWorkload(studentClass);
+                if (verbal) System.out.print(" [" + studentWorkload + "] " );
+            }
+        } else {
+            logShowErr("[Error] student's class is not a number");
+        }
+    }
+    
+    public void validateL3(String line){
+
+        if (line.trim().length()<6) { // 1d check
+            logShowErr("[Error] the student “number” must be a minimum of 6 characters ");
+        } else {
+            String first2characters_numbers = line.substring(0, 2);
+            int studentYear = Integer.parseInt(first2characters_numbers); //with the first 2 characters being numbers, 
+            if (studentYear<20) { // DW1 check: Ensure that the student number year is at least 2020 (i.e. that the number starts with 20 or higher)
+                logShowErr("[Error] the year is not valid in students's number (Must be 20 or higher)");
+            } else {
+                char L1 = line.charAt(2);
+                char L2 = line.charAt(3);
+                char L3 = line.charAt(4);
+
+                if (Character.isLetter(L1) && Character.isLetter(L2)){ //the 3rd  and 4th characters (and possibly 5th ) being a letter
+
+                    int pos = 4;
+                    if (Character.isLetter(L3)) pos = 5;
+                    String reasonableNumber = line.substring(pos);
+                    int intReasonableNumber = Integer.parseInt(reasonableNumber);
+                    if (intReasonableNumber<1 || intReasonableNumber>200) {
+                        logShowErr("[Error] students's number " + intReasonableNumber + " is not reasonable (must be between 1 and 200)");
+                    } else {
+                        System.out.print(first2characters_numbers + " " + reasonableNumber);
+                        StudentNumber = line;
+                    }
+
+                } else {
+                    logShowErr("[Error] the 3rd  and 4th characters (and possibly 5th ) in student's number must be a letter");
+                }
+
+            }
+        }
+    }
+    
     public void write2File(String studentData, boolean append ){
         try {
             outputF  = new BufferedWriter(new FileWriter(myOutFile, append ));
